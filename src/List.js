1d17a4f5dc6ed4e520038d654d327cc5ba2882cf
Seha Gulnar Dalay <sehagulnar@gmail.com>
2019-05-02 14:13:40 -0400

    Added to do items to the list

--- src/List.js
+++ src/List.js
@@ -1,23 +1,52 @@
 import React, { Component } from "react";
+import Items from "./Items";
 
 class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: []
+        };
+        this.addItem = this.addItem.bind(this);
+    }
+    addItem(e) {
+        if (this.inputElement.value !== "") {
+            let newItem = {
+                text: this.inputElement.value,
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+
+            this.inputElement.value = "";
+        }
+
+        console.log(this.state.items);
+
+        e.preventDefault();
+
+    }
     render() {
         return (
-            <div className="todoListMain">
-                <div className="header">
-                    <form>
-                        <input 
-                            type="text"
-                            name="todo_input_area"
-                            id="todo-input-area"
-                            placeholder="Add a todo here"
-                            size="30"
-                            required
-                        />
-                        <button type="submit">Add</button>
-                    </form>
-                </div>
+          <div className="todoListMain">
+            <div className="header">
+              <form onSubmit={this.addItem}>
+                <input ref={(a) => this.inputElement = a} 
+                  type="text"
+                  name="todo_input_area"
+                  id="todo-input-area"
+                  placeholder="Add a todo here"
+                  size="30"
+                  required
+                />
+                <button type="submit">Add</button>
+              </form>
             </div>
+                <Items entries={this.state.items} />
+          </div>
         );
     }
 }
